0dab8937fdc39b70191d872bce734fc34a38590f
Ron van Bree <ronvbree@hotmail.com>
2015-11-30 20:04:13 +0100

    Aanpassing aan controller

--- InteractiveLearner/src/controller/Controller.java
+++ InteractiveLearner/src/controller/Controller.java
@@ -1,8 +1,5 @@
 package controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import model.Classification;
 import model.Document;
 import model.classifiers.Classifier;
@@ -19,8 +16,6 @@ import view.View;
 public class Controller {
 	
 	public static void main(String[] args) {
-		List<Document> toProcess = new ArrayList<Document>();
-		
 		Classifier classifier = new NaiveBayes();
 		User user = new HumanUser();
 		Comparator comparator = new StandardComparator(user);
@@ -28,14 +23,16 @@ public class Controller {
 		View view = new TestUI();
 		
 		documentSet.reset();
-		for (Document document : toProcess)	{
-			classifier.train(documentSet);
-			Classification classification = classifier.classify(document);
-			Classification correctClassification = comparator.compare(document, classification);
-			documentSet.put(document, correctClassification);
+		view.update();
+		
+		while (true)	{
+			Document document = user.pickDocument();												/* Ask user to pick a document 		*/
+			classifier.train(documentSet);															/* Train classifier on document set */
+			Classification classification = classifier.classify(document);							/* Classify document 				*/
+			Classification correctClassification = comparator.compare(document, classification);	/* Ask feedback from user 			*/
+			documentSet.put(document, correctClassification);										/* Add to processed documents	 	*/
+			view.update();																			/* Show progress 					*/
 		}
-		
-		
 		
 		
 	}

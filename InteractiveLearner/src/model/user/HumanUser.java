26784da9a54b349da80c3615fe8c03bce347c539
Ron van Bree <ronvbree@hotmail.com>
2015-12-02 12:17:40 +0100

    Human classify

--- InteractiveLearner/src/model/user/HumanUser.java
+++ InteractiveLearner/src/model/user/HumanUser.java
@@ -3,31 +3,24 @@ package model.user;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import model.Classification;
 import model.Document;
 
 public class HumanUser implements User {
 	
-	private static final String moveInputFormat = "TBA"; // TODO
-	private static final Pattern regex = Pattern.compile(moveInputFormat);
+	private static final String CONFIRMATION = "yes";
+	private static final String DISAGREE = "no";
 	
 	@Override
 	public String getInput() {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		String input = "";
-		boolean valid = false;
-		do {
-			try {
-				input = reader.readLine();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			Matcher matcher = regex.matcher(input);
-			valid = matcher.matches();
-		} while (!valid);
+		try {
+			input = reader.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return input;
 	}
 
@@ -39,8 +32,13 @@ public class HumanUser implements User {
 
 	@Override
 	public boolean classify(Document document, Classification classification) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean valid = false;
+		String input = "";
+		do {
+			input = this.getInput();
+			valid = input.equals(CONFIRMATION) || input.equals(DISAGREE);
+		} while (!valid);
+		return input.equals(CONFIRMATION);
 	}
 
 }

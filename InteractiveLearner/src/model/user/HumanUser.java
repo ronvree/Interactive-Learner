057b27cd66fdca8ae9ff4eac345f5e1eb150a200
Ron van Bree <ronvbree@hotmail.com>
2015-12-02 11:56:57 +0100

    HumanUser askInput

--- InteractiveLearner/src/model/user/HumanUser.java
+++ InteractiveLearner/src/model/user/HumanUser.java
@@ -1,14 +1,34 @@
 package model.user;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import model.Classification;
 import model.Document;
 
 public class HumanUser implements User {
-
+	
+	private static final String moveInputFormat = "TBA"; // TODO
+	private static final Pattern regex = Pattern.compile(moveInputFormat);
+	
 	@Override
 	public String getInput() {
-		// TODO Auto-generated method stub
-		return null;
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String input = "";
+		boolean valid = false;
+		do {
+			try {
+				input = reader.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			Matcher matcher = regex.matcher(input);
+			valid = matcher.matches();
+		} while (!valid);
+		return input;
 	}
 
 	@Override

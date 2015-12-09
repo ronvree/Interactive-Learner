4c070f9b5859dbe76dd1855d071a3c9600f3f09c
Ron van Bree <ronvbree@hotmail.com>
2015-12-09 09:28:59 +0100

    Commented ProcessedDocumentSet Interface + Added some methods

--- InteractiveLearner/src/model/processeddocumentset/ProcessedDocumentSet.java
+++ InteractiveLearner/src/model/processeddocumentset/ProcessedDocumentSet.java
@@ -10,16 +10,50 @@ public interface ProcessedDocumentSet {
 	
 	public ProcessedDocument getDocument(int index);
 	
+	/**
+	 * Counts the total amount of occurrences of this word in all documents.
+	 * @param word -- The word to count
+	 */
 	public int getFrequency(String word);
 	
+	/**
+	 * Counts the amount of occurrences of this word in documents with the specified classification.
+	 * @param word -- The word to count
+	 * @param classification -- Classification of the documents
+	 */
 	public int getFrequency(String word, String classification);
 	
+	/**
+	 * Returns the amount of documents processed by this set
+	 */
 	public int size();
 	
+	/**
+	 * Returns the amount of words in this set
+	 */
+	public int wordCount();
+	
+	/**
+	 * Counts the amount of documents in this set with the specified classification
+	 * @param classification -- The classification of the document
+	 */
+	public int countDocuments(String classification);
+	
+	/**
+	 * Put a new document in the set
+	 * @param document -- document
+	 * @param classification -- its classification
+	 */
 	public void put(Document document, String classification);
 	
+	/**
+	 * Clear the learned data and re-learn from the source files
+	 */
 	public void reset();
 	
+	/**
+	 * Clear the learned data
+	 */
 	public void clear();
 	
 }

96905c05031e61fdd1414289ed72d40a57f02afa
GijsBeernink <g.j.beernink@student.utwente.nl>
2015-12-09 22:49:32 +0100

    testen naiveBayes doet
    testclass die je aan moet roepen: PickDocumentTest.java

--- InteractiveLearner/src/testing/PickDocumentTest.java
+++ InteractiveLearner/src/testing/PickDocumentTest.java
@@ -2,6 +2,7 @@ package testing;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 
 import model.classifiers.Classifier;
 import model.classifiers.NaiveBayes;
@@ -12,13 +13,25 @@ public class PickDocumentTest {
 	
 	public static void main(String[] args) {
 
-
+		System.out.println("START");
+		long time = System.currentTimeMillis();
 		File f1 = new File("InteractiveLearner"+File.separator+"data"+File.separator+"spam");
 		File f2 = new File("InteractiveLearner"+File.separator+"data"+File.separator+"ham");
 		BinomialDataSet docset = new BinomialDataSet(f1,f2);
 		Classifier NB = new NaiveBayes("spam", "ham");
 		NB.train(docset);
-		NB.classify(new StandardDocument(new File("InteractiveLearner"+File.separator+"data"+File.separator+"spam"+File.separator+"spmsga1.txt")));
+
+		File testSet = new File("InteractiveLearner"+File.separator+"data"+File.separator+"testSet");
+		int counter = 1;
+		for (File file : testSet.listFiles()) {
+			System.out.println("File " + counter + " of " + testSet.listFiles().length);
+			counter++;
+			System.out.println("Filename: " + file.getName());
+			NB.classify(new StandardDocument(file));
+			System.out.println();
+		}
+		System.out.println("Done! This took: " + (System.currentTimeMillis() - time));
 	}
+
 	
 }

2a701258ef4f78934c0709e95ef604bfd415bdae
GijsBeernink <g.j.beernink@student.utwente.nl>
2016-01-06 23:35:28 +0100

    data/blogTestData

--- InteractiveLearner/src/view/GUI.java
+++ InteractiveLearner/src/view/GUI.java
@@ -1,13 +1,9 @@
 package view;
 
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.io.File;
+import controller.Controller;
+import java.awt.event.*;
+import java.awt.*;
+import java.io.*;
 import java.util.HashMap;
 
 import javax.swing.JButton;
@@ -18,7 +14,6 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-import controller.Controller;
 
 
 public class GUI extends JPanel implements ActionListener {
@@ -101,8 +96,6 @@ public class GUI extends JPanel implements ActionListener {
 
                 System.out.println("test: " + testClassFolder);
 
-//                result = Controller.runTest(new File(firstClassFolder), firstClassNameValue, new File(secondClassFolder), secondClassNameValue, new File(testClassFolder), featureSelection.isSelected(), removeStopWords.isSelected());
-//                showResults();
                 Controller.runTest(new File(firstClassFolder), firstClassNameValue, new File(secondClassFolder), secondClassNameValue, new File(testClassFolder), featureSelection.isSelected(), removeStopWords.isSelected());
             } else {
                 String message = "Not entered correct data!";

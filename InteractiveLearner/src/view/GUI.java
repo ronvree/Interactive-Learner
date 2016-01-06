5c1a35991b0924a2d3e6c6f6997374e058f296a2
Ron van Bree <ronvbree@hotmail.com>
2016-01-06 23:20:46 +0100

    onnodige imports

--- InteractiveLearner/src/view/GUI.java
+++ InteractiveLearner/src/view/GUI.java
@@ -1,11 +1,13 @@
 package view;
 
-import controller.Controller;
-import javax.swing.*;
-import java.awt.event.*;
-import java.awt.*;
-import java.io.*;
-import java.util.ArrayList;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
 import java.util.HashMap;
 
 import javax.swing.JButton;
@@ -14,7 +16,6 @@ import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
 import controller.Controller;

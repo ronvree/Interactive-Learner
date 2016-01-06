f575c6b9145d05ade2b9fddd9081c92fa04cb9ba
Ron van Bree <ronvbree@hotmail.com>
2016-01-06 20:02:40 +0100

    WritingChiSquared (schrijft waarden van Chi Squared in bestand)

--- InteractiveLearner/src/testing/TestFeatureSelection.java
+++ InteractiveLearner/src/testing/TestFeatureSelection.java
@@ -3,7 +3,6 @@ package testing;
 import java.io.File;
 import java.util.List;
 
-import model.featureselection.ChiSquared;
 import model.featureselection.FeatureSelection;
 import model.processeddocumentset.BinomialDataSet;
 import model.processeddocumentset.ProcessedDocumentSet;
@@ -17,12 +16,16 @@ public class TestFeatureSelection {
 		String c2 = "spam";
 		
 		ProcessedDocumentSet dataset = new BinomialDataSet(new File(location + c1), new File(location + c2));
-		FeatureSelection fs = new ChiSquared(dataset);
+		FeatureSelection fs = new WritingChiSquared(dataset);
 		
 		fs.update();
 		
-		List<String> selected = fs.getSelectedWords("spam");
-		for (String word : selected)	{
+		List<String> selected1 = fs.getSelectedWords(c1);
+		List<String> selected2 = fs.getSelectedWords(c2);
+		for (String word : selected1)	{
+			System.out.println(word);
+		}
+		for (String word : selected2)	{
 			System.out.println(word);
 		}
 		
